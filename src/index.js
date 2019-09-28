3a7487af4b09d44440ada8b2ff5a3ea5650f81f1
r3850355 <r3850355@gmail.com>
2019-09-28 16:52:31 +0800

    imagemap message update

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ const linebot = require('linebot')
 require('dotenv').config()
 
 const app = express()
+app.use('/statics', express.static(__dirname + '/statics'))
 const bot = new linebot({
   channelId: process.env.channelId,
   channelSecret: process.env.channelSecret,
   channelAccessToken: process.env.channelAccessToken
 })
 // 程式碼都寫在下面這個區塊內
-bot.on('message', async event => {
-  if (event.message.type === 'text') {
-    event.reply(event.message.text)
-  }
-})
+
+
 // 程式碼都寫在這個區塊內 ＾＾＾
 app.post('/webhook', bot.parser())
 app.listen(3000, res => {
