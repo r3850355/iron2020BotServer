05eb1030437e3694cf202f44e96f0274004abc02
r3850355 <r3850355@gmail.com>
2019-09-28 17:30:41 +0800

    update

--- src/imageMap.js
+++ src/imageMap.js
@@ -4,16 +4,37 @@ const linebot = require('linebot')
 require('dotenv').config()
 
 const app = express()
+app.use('/statics', express.static(__dirname + '/statics'))
 const bot = new linebot({
   channelId: process.env.channelId,
   channelSecret: process.env.channelSecret,
   channelAccessToken: process.env.channelAccessToken
 })
 // 程式碼都寫在下面這個區塊內
+// 訊息事件
 bot.on('message', async event => {
-  if (event.message.type === 'text') {
-    event.reply(event.message.text)
+  let msg = {
+    "type": "imagemap",
+    "baseUrl": "https://c989ee6a.ngrok.io/statics/flight",
+    "altText": "航班資訊",
+    "baseSize": {
+      "width": 1040,
+      "height": 800
+    },
+    "actions": [
+      {
+        "type": "message",
+        "area": {
+          "x": 584,
+          "y": 613,
+          "width": 456,
+          "height": 187
+        },
+        "text": "航班資訊"
+      }
+    ]
   }
+  event.reply(msg)
 })
 // 程式碼都寫在這個區塊內 ＾＾＾
 app.post('/webhook', bot.parser())
